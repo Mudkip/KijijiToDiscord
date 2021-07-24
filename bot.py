94ab677e6c9cd17e7b36c123a9bfa692710e9e73
Mudkip <me@mudkip.pw>
2021-07-24 17:57:12 -0230

    Style and formatting

--- bot.py
+++ bot.py
@@ -11,68 +11,79 @@ from textwrap import dedent
 from discord.ext import commands, tasks
 from scraper import Scraper
 
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix="?")
 guild_channels = {}
 scrape_urls = {}
 
+
 @bot.event
 async def on_ready():
-    print('Starting up KijijiBot.')
-    print('Initializing SQLite tables.')
+    print("Starting up KijijiBot.")
+    print("Initializing SQLite tables.")
     try:
-        bot.db = sqlite3.connect(os.getenv('DB_PATH'))
-        bot.db.execute("""
+        bot.db = sqlite3.connect(os.getenv("DB_PATH"))
+        bot.db.execute(
+            """
             CREATE TABLE IF NOT EXISTS guild_channels (
                 id INTEGER PRIMARY KEY,
                 guild VARCHAR(32) UNIQUE NOT NULL,
                 channel VARCHAR(32) NOT NULL
             )
-        """)
-        bot.db.execute("""
+        """
+        )
+        bot.db.execute(
+            """
             CREATE TABLE IF NOT EXISTS track_urls (
                 id INTEGER PRIMARY KEY,
                 guild VARCHAR(32) NOT NULL,
                 url VARCHAR(128) NOT NULL
             )
-        """)
-        bot.db.execute("""
+        """
+        )
+        bot.db.execute(
+            """
             CREATE TABLE IF NOT EXISTS keyword_pings (
                 id INTEGER PRIMARY KEY,
                 user VARCHAR(64) NOT NULL,
                 keyword VARCHAR(32) NOT NULL
             )
-        """)
-        bot.db.execute("""
+        """
+        )
+        bot.db.execute(
+            """
             CREATE TABLE IF NOT EXISTS seen_ads (
                 ad_id INT NOT NULL,
                 guild VARCHAR(32) NOT NULL,
                 timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 PRIMARY KEY (ad_id, guild)
             )
-        """)
+        """
+        )
         bot.db.commit()
-        print('SQLite tables initialized.')
+        print("SQLite tables initialized.")
     except Exception as e:
-        print(f'Unexpected error while loading SQLite database: \n {e}')
+        print(f"Unexpected error while loading SQLite database: \n {e}")
 
     cur = bot.db.cursor()
-    print('Loading guilds and their channels')
-    for guild_id, channel_id in cur.execute('SELECT guild, channel FROM guild_channels'):
-            guild = bot.get_guild(int(guild_id))
-            channel = bot.get_channel(int(channel_id))
-            guild_channels[guild] = channel
-            
+    print("Loading guilds and their channels")
+    for guild_id, channel_id in cur.execute(
+        "SELECT guild, channel FROM guild_channels"
+    ):
+        guild = bot.get_guild(int(guild_id))
+        channel = bot.get_channel(int(channel_id))
+        guild_channels[guild] = channel
 
-    print('Loading scrape URLs')
-    for guild_id, url in cur.execute('SELECT guild, url FROM track_urls'):
+    print("Loading scrape URLs")
+    for guild_id, url in cur.execute("SELECT guild, url FROM track_urls"):
         guild = bot.get_guild(int(guild_id))
         if not (guild in scrape_urls):
             scrape_urls[guild] = []
         scrape_urls[guild].append(url)
 
-    print('Scheduling scraper task.')
+    print("Scheduling scraper task.")
     run_scraper.start()
-    print('All done!')
+    print("All done!")
+
 
 @tasks.loop(minutes=5)
 async def run_scraper():
@@ -80,40 +91,56 @@ async def run_scraper():
     await scraper.execute()
     cur = bot.db.cursor()
     for guild, ads in scraper.get_ads().items():
-        if(await get_ad_dump_channel(guild)):
+        if await get_ad_dump_channel(guild):
             chan = await get_ad_dump_channel(guild)
             for ad_map in ads:
-                url = ad_map['url']
-                ad_list = ad_map['ads']
+                url = ad_map["url"]
+                ad_list = ad_map["ads"]
                 for ad in ad_list:
-                    ad_id = int(ad['id'])
-                    cur.execute('SELECT EXISTS(SELECT 1 FROM seen_ads WHERE ad_id=? AND guild=? LIMIT 1)', (ad_id, guild.id))
+                    ad_id = int(ad["id"])
+                    cur.execute(
+                        "SELECT EXISTS(SELECT 1 FROM seen_ads WHERE ad_id=? AND guild=? LIMIT 1)",
+                        (ad_id, guild.id),
+                    )
                     exists = cur.fetchone()[0]
                     if exists == 0:
-                        bot.db.execute('INSERT INTO seen_ads(ad_id, guild) VALUES(?,?)', (ad_id, guild.id))
+                        bot.db.execute(
+                            "INSERT INTO seen_ads(ad_id, guild) VALUES(?,?)",
+                            (ad_id, guild.id),
+                        )
                         await chan.send(await format_ad(ad))
                 bot.db.commit()
-                        
+
 
 @bot.command()
 async def addurl(ctx, url: str):
     """Adds a url for kijiji bot to track"""
-    
+
     if ctx.message.guild in scrape_urls and url in scrape_urls[ctx.message.guild]:
-        await ctx.send('This URL is already being tracked.')
-    elif re.match(r'^https:\/\/www\.kijiji\.ca\/b-[\w+-]+(\/[\w+-]+)?(\/[\w+-]+)?\/[\w+-]+$', url):
-        res = requests.get(url, headers={'User-Agent': os.getenv('USER_AGENT')})
-        if res.status_code == 200 and 'showing' in res.text:
-                bot.db.execute('INSERT INTO track_urls(guild, url) VALUES(?,?)', (ctx.message.guild.id, url))
-                bot.db.commit()
-                if not (ctx.message.guild in scrape_urls):
-                    scrape_urls[ctx.message.guild] = []
-                scrape_urls[ctx.message.guild].append(url)
-                await ctx.send('Added URL for tracking!')
+        await ctx.send("This URL is already being tracked.")
+    elif re.match(
+        r"^https:\/\/www\.kijiji\.ca\/b-[\w+-]+(\/[\w+-]+)?(\/[\w+-]+)?\/[\w+-]+$", url
+    ):
+        res = requests.get(url, headers={"User-Agent": os.getenv("USER_AGENT")})
+        if res.status_code == 200 and "showing" in res.text:
+            bot.db.execute(
+                "INSERT INTO track_urls(guild, url) VALUES(?,?)",
+                (ctx.message.guild.id, url),
+            )
+            bot.db.commit()
+            if not (ctx.message.guild in scrape_urls):
+                scrape_urls[ctx.message.guild] = []
+            scrape_urls[ctx.message.guild].append(url)
+            await ctx.send("Added URL for tracking!")
         else:
-            await ctx.send('Cannot parse Kijiji URL. Make sure that the URL you provided actually shows ads.')
+            await ctx.send(
+                "Cannot parse Kijiji URL. Make sure that the URL you provided actually shows ads."
+            )
     else:
-        await ctx.send('Invalid Kijiji URL. Make sure you are copying the URL from your browser and that there are no extra query params.')
+        await ctx.send(
+            "Invalid Kijiji URL. Make sure you are copying the URL from your browser and that there are no extra query params."
+        )
+
 
 @bot.command()
 async def listurls(ctx):
@@ -124,48 +151,67 @@ async def listurls(ctx):
             to_send = to_send + "<" + url + ">\n"
         await ctx.send(to_send)
     else:
-        await ctx.send('There are no Kijiji URLs currently being tracked for this guild.')
+        await ctx.send(
+            "There are no Kijiji URLs currently being tracked for this guild."
+        )
+
 
 @bot.command()
 async def removeurl(ctx, url: str):
     """Removes a URL from tracking"""
     if ctx.message.guild in scrape_urls and url in scrape_urls[ctx.message.guild]:
         scrape_urls[ctx.message.guild].remove(url)
-        bot.db.execute("DELETE FROM track_urls WHERE url=? AND guild=?", (url, ctx.message.guild.id))
+        bot.db.execute(
+            "DELETE FROM track_urls WHERE url=? AND guild=?",
+            (url, ctx.message.guild.id),
+        )
         bot.db.commit()
-        await ctx.send('That URL won\'t be tracked anymore')
+        await ctx.send("That URL won't be tracked anymore")
     else:
-        await ctx.send('Nothing to do. URL is not being tracked.')
+        await ctx.send("Nothing to do. URL is not being tracked.")
+
 
 @bot.command()
 async def setchannel(ctx, channel: discord.TextChannel = None):
     """Sets the channel for the ad dumps"""
-    if not (ctx.message.guild in guild_channels) or guild_channels[ctx.message.guild] != channel:
-        bot.db.execute('INSERT OR REPLACE INTO guild_channels(guild, channel) VALUES(?,?)', (ctx.message.guild.id, channel.id))
+    if (
+        not (ctx.message.guild in guild_channels)
+        or guild_channels[ctx.message.guild] != channel
+    ):
+        bot.db.execute(
+            "INSERT OR REPLACE INTO guild_channels(guild, channel) VALUES(?,?)",
+            (ctx.message.guild.id, channel.id),
+        )
         bot.db.commit()
         guild_channels[ctx.message.guild] = channel
-        await ctx.send(f'Done! All ads will be sent to {channel.mention}.')
+        await ctx.send(f"Done! All ads will be sent to {channel.mention}.")
     else:
-        await ctx.send(f'All ads are already going into {channel.mention}.')
-        
+        await ctx.send(f"All ads are already going into {channel.mention}.")
+
+
 @setchannel.error
 async def setchannel_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.ChannelNotFound):
-        await ctx.send('Channel could not be found and was not set as a result.')
+        await ctx.send("Channel could not be found and was not set as a result.")
+
 
 async def get_ad_dump_channel(guild):
     if guild in guild_channels:
         return guild_channels[guild]
     return None
 
+
 async def format_ad(ad_dic):
-    return dedent(f"""
+    return dedent(
+        f"""
         ===========================================
         :newspaper: **Kijiji Ad - {ad_dic['title']}!**
         Title: ``{ad_dic['title']}``
         Price: ``{ad_dic['price']}``
         Description:```{ad_dic['desc']}```
         {ad_dic['url']}
-    """)
+    """
+    )
 
-bot.run(os.getenv('DISCORD_TOKEN'))
+
+bot.run(os.getenv("DISCORD_TOKEN"))
--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
 
+
 class Scraper:
     def __init__(self, guild_scrapes):
         self.guild_scrapes = guild_scrapes
@@ -14,7 +15,7 @@ class Scraper:
 
     def get_ads(self):
         return self.ad_map
-        
+
     async def fetch(self, session, guild, url):
         try:
             async with session.get(url) as res:
@@ -22,29 +23,43 @@ class Scraper:
                 ads = await self.extract_ads(text)
                 return url, guild, ads
         except Exception as e:
-            print(f'Error fetching ads from url {url}: ' + str(e))
+            print(f"Error fetching ads from url {url}: " + str(e))
 
     async def extract_ads(self, text):
-        soup = BeautifulSoup(text, 'html.parser')
+        soup = BeautifulSoup(text, "html.parser")
         ad_divs = soup.find_all("div", class_="search-item")
         ads = []
         for ad in ad_divs:
             info = ad.find("div", class_="info-container")
-            desc = info.find("div", class_="description").find(text=True, recursive=False).strip()
-            title = info.find("a", class_="title").find(text=True, recursive=False).strip()
-            price = info.find("div", class_="price").find(text=True, recursive=False).strip()
-            ads.append({
-                'id': ad['data-listing-id'],
-                'url': 'https://www.kijiji.ca' + ad['data-vip-url'],
-                'title': title.replace('`','\'').replace('*', ''),
-                'price': price if price else 'Not specified',
-                'desc': desc.replace('`','\'') if desc else 'Not specified'
-            })
+            desc = (
+                info.find("div", class_="description")
+                .find(text=True, recursive=False)
+                .strip()
+            )
+            title = (
+                info.find("a", class_="title").find(text=True, recursive=False).strip()
+            )
+            price = (
+                info.find("div", class_="price")
+                .find(text=True, recursive=False)
+                .strip()
+            )
+            ads.append(
+                {
+                    "id": ad["data-listing-id"],
+                    "url": "https://www.kijiji.ca" + ad["data-vip-url"],
+                    "title": title.replace("`", "'").replace("*", ""),
+                    "price": price if price else "Not specified",
+                    "desc": desc.replace("`", "'") if desc else "Not specified",
+                }
+            )
         return ads
-    
+
     async def execute(self):
         tasks = []
-        async with aiohttp.ClientSession(headers={'User-Agent': os.getenv('USER_AGENT')}) as session:
+        async with aiohttp.ClientSession(
+            headers={"User-Agent": os.getenv("USER_AGENT")}
+        ) as session:
             for guild, urls in self.guild_scrapes.items():
                 for url in urls:
                     tasks.append(self.fetch(session, guild, url))
@@ -55,4 +70,4 @@ class Scraper:
                 if ads is not None:
                     if not (ads[1] in self.ad_map):
                         self.ad_map[ads[1]] = []
-                    self.ad_map[ads[1]].append({'url': ads[0], 'ads': ads[2]})
+                    self.ad_map[ads[1]].append({"url": ads[0], "ads": ads[2]})
